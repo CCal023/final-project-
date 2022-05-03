3b6a19ca24a18479f36510c227052ec76ec7e232
CCal023 <104841739+CCal023@users.noreply.github.com>
2022-05-03 15:29:41 -0400

    Add files via upload

---
+++ Final_Project.py
@@ -0,0 +1,7 @@
+print("Hello world")
+def gamestart():
+    print("Welcome to the game ")
+class chracter():
+    def __init__(self, strength, speed, intellegence,):
+        self.strength= strch
+        
